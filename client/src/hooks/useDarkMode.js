b021107f52e67d69d7774533600ede72bf15b888
adriancobon <49730380+adriancobon@users.noreply.github.com>
2019-12-15 18:15:30 -0500

    Modified app.css to include dark-mode info. Also create the hooks folder and useDarkMode.js

---
+++ client/src/hooks/useDarkMode.js
@@ -0,0 +1,19 @@
+// The purpose of this file it to create a function that changes the background color. Creating this before localstorage due to relative easiness...
+
+import { useEffect } from 'react';
+// I know I will need local storage in a hot sec, so creating it now...
+import useLocalStorage from './useLocalStorage';
+
+cont useDarkMode = (initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage('dark-mode', initialValue);
+    useEffect (() => {
+        if (darkMode) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    }, [darkMode]);
+    return [darkMode, setDarkMode];
+};
+
+export default useDarkMode;
